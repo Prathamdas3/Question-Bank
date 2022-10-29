6a4fa3febbe52243d068837ab578f3d7530ade32
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2022-10-29 10:51:23 -0500

    add logic to the practice file

---
+++ practice.js
@@ -0,0 +1,5 @@
+"use strict";
+document.querySelector(".back").addEventListener("click", function () {
+  window.location.href = "main.html";
+});
+document.querySelector("h2").addEventListener("click", function () {});
