400ad1678178f9ff2426532043de4067ec253869
Prathamdas3 <116100433+Prathamdas3@users.noreply.github.com>
2022-10-29 10:43:53 -0500

    add logic to the questions to work like a button

--- learn.js
+++ learn.js
@@ -2,3 +2,6 @@
 document.querySelector(".back").addEventListener("click", function () {
   window.location.href = "main.html";
 });
+document.querySelector(".ex").addEventListener("click", function () {
+  window.location.href = "question.html";
+});
